d3596d325e1c9d3d69a385116de7db66581f8f4b
hipes <ansj2916@gmail.com>
2023-09-21 13:16:36 +0900

    replydao replydto 추가

---
+++ src/main/java/dao/ReplyDAO.java
@@ -0,0 +1,110 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+import dto.ReplyDTO;
+
+
+public class ReplyDAO {
+	private static ReplyDAO instantce;
+	
+	public static ReplyDAO getInstance() {
+		if(instantce==null) {
+			instantce=new ReplyDAO();
+		}
+		return instantce;
+	}
+	
+	public Connection getConnection() throws Exception{
+		Context ctx=new InitialContext();
+		DataSource ds=(DataSource)ctx.lookup("java:comp/env/jdbc/mysql");
+		return ds.getConnection();
+	}
+	
+	public int writeReply(ReplyDTO dto) throws  Exception {
+		String sql="insert into reply values(null,?,?,?,?)";
+		try(Connection con=this.getConnection();
+			PreparedStatement pstat=con.prepareStatement(sql);){
+			pstat.setString(1,dto.getWriter());
+			pstat.setString(2,dto.getContents());
+			pstat.setTimestamp(3, dto.getWrite_date());
+			pstat.setInt(4,dto.getParent_seq());
+			
+			return pstat.executeUpdate();
+		}
+	}
+	
+	public boolean isReplyExist(int parentSeq) throws Exception {
+		String sql="select seq from reply where parent_seq =?";
+		try(Connection con=this.getConnection();
+			PreparedStatement pstat=con.prepareStatement(sql);){
+			pstat.setInt(1, parentSeq);
+			ResultSet rs = pstat.executeQuery();
+			return rs.next();
+		}
+	}
+	
+	public List<ReplyDTO> selectAll() throws Exception{
+		String sql="select * from reply";
+		try (Connection con = this.getConnection();
+				PreparedStatement stat = con.prepareStatement(sql);
+				ResultSet rs=stat.executeQuery();){
+			List<ReplyDTO>list =new ArrayList<>();
+			while(rs.next()) {
+				int seq=rs.getInt(1);
+				String writer=rs.getString(2);
+				String contents=rs.getString(3);
+				Timestamp date=rs.getTimestamp(4);
+				int parentseq=rs.getInt(5);
+				list.add(new ReplyDTO(seq, writer, contents,date,parentseq));
+			}
+			return list;
+		}
+	}
+	
+	public int deleteReply(int seq) throws Exception {
+		String sql="delete from reply where seq=?";
+		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
+			pstat.setInt(1, seq);
+			return pstat.executeUpdate();
+		}
+	}
+	
+	public int upDateContents(int seq, String contents) throws Exception {
+		String sql = "update reply set contents=? where seq=? ";
+		try (Connection con = this.getConnection(); PreparedStatement pstat = con.prepareStatement(sql);) {
+			pstat.setString(1, contents);
+			pstat.setInt(2, seq);
+			return pstat.executeUpdate();
+		}
+	}
+	
+	public List<ReplyDTO> selectReply(int seq) throws Exception{
+		String sql="select * from reply  where parent_seq=?";
+		try (Connection con = this.getConnection();
+			PreparedStatement stat = con.prepareStatement(sql);){
+			stat.setInt(1, seq);
+			List<ReplyDTO>list =new ArrayList<>();
+			ResultSet rs=stat.executeQuery();
+			while(rs.next()) {
+				int pseq=rs.getInt(1);
+				String writer=rs.getString(2);
+				String contents=rs.getString(3);
+				Timestamp date=rs.getTimestamp(4);
+				int parentseq=rs.getInt(5);
+				list.add(new ReplyDTO(pseq, writer, contents,date,parentseq));
+			}
+			return list;
+		}
+	}
+}
---
+++ src/main/java/dto/ReplyDTO.java
@@ -0,0 +1,80 @@
+package dto;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+
+public class ReplyDTO {
+	private int seq;
+	private String writer;
+	private String contents;
+	private Timestamp write_date;
+	private int parent_seq;
+	
+	
+	
+	public ReplyDTO() {}
+	
+	
+	public ReplyDTO(int seq, String writer, String contents, Timestamp write_date, int parent_seq) {
+		super();
+		this.seq = seq;
+		this.writer = writer;
+		this.contents = contents;
+		this.write_date = write_date;
+		this.parent_seq = parent_seq;
+	}
+	
+	
+	public String getFormedSignupData() {
+		long currentTime = System.currentTimeMillis();
+		long signup = this.write_date.getTime();
+
+		long gapTime = currentTime - signup;
+
+		if (gapTime < 60000) {
+			return gapTime / 1000 + "초 전";
+		} else if (gapTime < 60000 * 60) {
+			return gapTime / 60000 + "분 전";
+		} else if (gapTime < 60000 * 60 * 24) {
+			return gapTime / (60000 * 60) + "시간 전";
+		} else {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일 HH:mm:ss");
+			return sdf.format(getWrite_date());
+		}
+	}
+	
+	
+	
+	
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContents() {
+		return contents;
+	}
+	public void setContents(String contents) {
+		this.contents = contents;
+	}
+	public Timestamp getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(Timestamp write_date) {
+		this.write_date = write_date;
+	}
+	public int getParent_seq() {
+		return parent_seq;
+	}
+	public void setParent_seq(int parent_seq) {
+		this.parent_seq = parent_seq;
+	}
+	
+}
